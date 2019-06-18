563233b0924a567c26a129d6af74cab87ae0ca0c
Keith Isham <kisham8899@gmail.com>
2019-06-18 22:40:39 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -43,5 +43,8 @@ end
 end
 
 def selects_the_category_name_and_the_sum_total_of_the_all_its_pledges_for_the_books_category
-
+  "SELECT projects.category, pledges.amount
+  FROM projects
+  JOIN pledges ON projects.id = pledges.project_id
+  WHERE projects.category = 'music';"
 end
